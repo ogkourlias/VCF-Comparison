d43d2a354f0dc66dd757c1afa174468b3901055d
ogkourlias <ogkourlias@gmail.com>
2024-02-16 09:13:26 +0000

    Changed argparse arguments and added docstring.

--- bin/get_region.py
+++ bin/get_region.py
@@ -2,9 +2,7 @@
 
 """
     usage:
-        ./vcf_rompare.py -f [INPUT VCF FILE] -r [VCF COMPARISON FILE]
-        -ih [INPUT HEADERS TEXT FILE] -ch [COMPARISON HEADER TEXT FILE]
-        -chr [CHROMOSOME TEXT FILE] -n [CHUNKSIZE (Variant amount per run)]
+        ./vcf_rompare.py -i [INPUT VCF.GZ FILE] -G [GTF REF FILE] -o [OUTPUT.TSV.GZ FILE]
 """
 
 # METADATA VARIABLES
@@ -50,28 +48,18 @@ def arg_parse():
     )
     parser.add_argument("-i", "--tsv", type=str)
     parser.add_argument("-g", "--gtf", type=str)
-    parser.add_argument("-c", "--chr", type=str)
-    parser.add_argument("-n", "--chunk_size", type=int, default=10000)
     parser.add_argument("-o", "--output", type=str)
     return parser.parse_args()
 
 class get_region:
-    def __init__(self, mode, tsv, gtf, chr, chunk_size, output):
+    def __init__(self, mode, tsv, gtf, output):
         self.mode = mode
         self.tsv = tsv
-        self.chunk_size = chunk_size
-        self.chr = int(chr[3:])
         self.output = output
         self.annotation = GTFAnnotation(gtf)
     
     def get_genes(self):
         self.genes_by_chr = self.annotation.getGenesByChromosome()
-
-    def get_transcripts(self):
-        ...
-
-    def get_exons(self):
-        ...
     
     def get_regions(self):
         with gzip.open(self.output, "wt") as fo:
@@ -175,45 +163,6 @@ class get_region:
                     variant.append(region_status)
                     variant_str = "\t".join(variant) + "\n"
                     fo.write(variant_str) 
-
-    def compare(self, variant_list, genes, fo):
-        for variant in variant_list:
-            pos = int(variant[0].split(":")[1])
-            overlapGenes = []
-            region_status = "nan"
-            gene_types = []
-            overlapTranscripts = []
-            overlapExons = []
-            for gene in genes:
-                if pos >= gene.start and pos <= gene.stop:
-                    overlapGenes.append(gene)
-            
-            if len(overlapGenes) > 0:
-                for gene in overlapGenes:
-                    gene_types.append(gene.type)
-                    transcripts = gene.transcripts
-                    for transcript in transcripts:
-                        if pos >= transcript.start and pos <= transcript.stop:
-                            overlapTranscripts.append(transcript.name)
-                            if region_status != "exonic":
-                                region_status = "intronic"
-                            exons = transcript.exons
-                            for exon in exons:
-                                if pos >= exon.start and pos <= exon.stop:
-                                    overlapExons.append(exon.name)
-                                    region_status = "exonic"
-            
-            overlapTranscripts = ":".join(overlapTranscripts)
-            overlapExons = ":".join(overlapExons)
-            gene_types = ":".join(gene_types)
-                    
-            
-            variant.append(gene_types)
-            variant.append(overlapTranscripts)
-            variant.append(overlapExons)
-            variant.append(region_status)
-            variant_str = "\t".join(variant) + "\n"
-            fo.write(variant_str)
     
 # MAIN
 def main(args):
@@ -226,7 +175,7 @@ def main(args):
     elif args.all:
         mode = "all"
 
-    getter = get_region(mode, args.tsv, args.gtf, args.chr, args.chunk_size, args.output)
+    getter = get_region(mode, args.tsv, args.gtf, args.output)
     getter.get_regions()
     # FINISH
     return 0
